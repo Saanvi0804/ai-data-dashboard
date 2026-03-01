324e15b4908e35336808832be2dfac752c950684
SAANVI SHETTY <saanvi.082005@gmail.com>
2026-03-01 11:51:42 +0530

    fixed database.py

--- backend/database.py
+++ backend/database.py
@@ -1,12 +1,16 @@
 from sqlalchemy import create_engine, Column, String, DateTime, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 import os
 from datetime import datetime
 
-DATABASE_URL = os.getenv("postgresql://ai_dashboard_db_user:F5yyLH7Z1GWPQkUSoqMcNiahStpAOZuK@dpg-d6hra9450q8c73aojlu0-a/ai_dashboard_db")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    connect_args={"sslmode": "require"},
+)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
